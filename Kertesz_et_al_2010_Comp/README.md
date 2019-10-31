b4cb4ce17a76426934f6f7fcf1482ce15a65015b
Charles Warden <cwarden45@gmail.com>
2019-10-31 15:09:10 -0700

    Add files via upload

---
+++ Kertesz_et_al_2010_Comp/README.md
@@ -0,0 +1,23 @@
+After creating the original repository, I learned about the study of [Kertesz et al. 2010](https://www.ncbi.nlm.nih.gov/pubmed/20811459).
+
+So, I was curious if I could high higher scores for within the EvoFold fRNA predictions (versus the rest of the coding regions)
+
+**1)** I converted the EvoFold annotations from sacCer1 to sacCer2 using the web-based [UCSC liftOver](https://genome.ucsc.edu/cgi-bin/hgLiftOver)
+
+**2)** Convert [GSE22393](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE22393) .wig file to .bed file using [bedOps wig2bed](https://bedops.readthedocs.io/en/latest/content/reference/file-management/conversion/wig2bed.html)
+
+```
+wig2bed < GSE22393_processed_merged_PARS_sacCer2_1.wig > GSE22393_processed_merged_PARS_sacCer2_1.bed
+```
+
+I was not exactly sure what to expect, but the higher PARS score does seem to be extended into the HAC1 fold that I described as the positive control in the [Warden et al. 2008](https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0001559) paper
+
+![HAC1 PARS Score](HAC1_PARS_EvoFold.png "PARS scores extends into folded region of intron")
+
+**3)** Then, I used an R script (`compare_predictions.R`) to create a plot to compare scores:
+
+The gene annotation file came from the [Supplemental Website](https://genie.weizmann.ac.il/pubs/PARS10/pars10_catalogs.html) for the paper.
+
+![Overall PARS Score Distributions](PARS_density.png "PARS Score Density Distributions")
+
+Perhaps this means I should try looking at the intronic / intergenic portions of partially overlapping EvoFold fold (like for HAC1).  However, I am not currently certain what to think of these density plots (except that they look similar with or without filtering for EvoFold overlap)
---
+++ Kertesz_et_al_2010_Comp/compare_predictions.R
@@ -0,0 +1,111 @@
+EvoFold.file = "EvoFold_liftOver_sacCer1-to-sacCer2.bed"
+PARS.file = "../Kertesz_et_al_2010/GSE22393_processed_merged_PARS_sacCer2_1.bed"
+genes.file = "../Kertesz_et_al_2010/sce_transcriptome_global.tab"
+
+EvoFold.table = read.table(EvoFold.file, head=F, sep="\t")
+PARS.table = read.table(PARS.file, head=F, sep="\t")
+genes.table = read.table(genes.file, head=F, sep="\t")
+
+exon.table = genes.table[genes.table$V5 == "Exon",]
+Roman.chr = rep(NA,nrow(exon.table))
+Roman.chr[exon.table$V1 == 1]="chrI"
+Roman.chr[exon.table$V1 == 2]="chrII"
+Roman.chr[exon.table$V1 == 3]="chrIII"
+Roman.chr[exon.table$V1 == 4]="chrIV"
+Roman.chr[exon.table$V1 == 5]="chrV"
+Roman.chr[exon.table$V1 == 6]="chrVI"
+Roman.chr[exon.table$V1 == 7]="chrVII"
+Roman.chr[exon.table$V1 == 8]="chrVIII"
+Roman.chr[exon.table$V1 == 9]="chrIX"
+Roman.chr[exon.table$V1 == 10]="chrX"
+Roman.chr[exon.table$V1 == 11]="chrXI"
+Roman.chr[exon.table$V1 == 12]="chrXII"
+Roman.chr[exon.table$V1 == 13]="chrXIII"
+Roman.chr[exon.table$V1 == 14]="chrXIV"
+Roman.chr[exon.table$V1 == 15]="chrXV"
+Roman.chr[exon.table$V1 == 16]="chrXVI"
+
+unstranded.start = rep(NA, nrow(exon.table))
+unstranded.start[exon.table$V4 >= exon.table$V3] = exon.table$V3[exon.table$V4 >= exon.table$V3]
+unstranded.start[exon.table$V3 > exon.table$V4] = exon.table$V4[exon.table$V3 > exon.table$V4]
+
+unstranded.stop = rep(NA, nrow(exon.table))
+unstranded.stop[exon.table$V4 >= exon.table$V3] = exon.table$V4[exon.table$V4 >= exon.table$V3]
+unstranded.stop[exon.table$V3 > exon.table$V4] = exon.table$V3[exon.table$V3 > exon.table$V4]
+
+exon.table = data.frame(Roman.chr, unstranded.start, unstranded.stop, exon.table)
+
+library("GenomicRanges")
+
+EvoFold_gr = GRanges(Rle(EvoFold.table$V1),
+    		IRanges(start=EvoFold.table$V2, end=EvoFold.table$V3))
+    		
+PARS_gr = GRanges(Rle(PARS.table$V1),
+    		IRanges(start=PARS.table$V2, end=PARS.table$V3))
+    		
+exon_gr = GRanges(Rle(exon.table$Roman.chr),
+    		IRanges(start=exon.table$unstranded.start, end=exon.table$unstranded.stop))
+    		
+EvoFold_PARS_gr = union(EvoFold_gr, PARS_gr)
+EvoFold_PARS.table = data.frame(EvoFold_PARS_gr)
+EvoFold.All.ID = paste(EvoFold_PARS.table$seqnames, EvoFold_PARS.table$start, sep=":")
+
+EvoFold_exon_gr = union(EvoFold_gr, exon_gr)
+EvoFold_exon.table = data.frame(EvoFold_exon_gr)
+EvoFold.exon.ID = paste(EvoFold_exon.table$seqnames, EvoFold_exon.table$start, sep=":")
+
+PARS_exon_gr = union(PARS_gr, exon_gr)
+PARS_exon.table = data.frame(PARS_exon_gr)
+PARS.exon.ID = paste(PARS_exon.table$seqnames, PARS_exon.table$start, sep=":")
+
+EvoFold_exons_PARS_gr = union(EvoFold_gr, PARS_exon_gr)
+EvoFold_exons_PARS.table = data.frame(EvoFold_exons_PARS_gr)
+EvoFold_PARS.exon.ID = paste(EvoFold_exons_PARS.table$seqnames, EvoFold_exons_PARS.table$start, sep=":")
+
+PARS.ID = paste(PARS.table$V1, PARS.table$V2, sep=":")
+
+png("PARS_density.png")
+par(mfcol=c(1,2))
+
+#################
+### all sites ###
+#################
+
+background = PARS.table$V5
+print(length(background))
+print(quantile(background))
+
+EvoFold = PARS.table$V5[match(EvoFold.All.ID,PARS.ID,nomatch=0)]
+print(length(EvoFold))
+print(quantile(EvoFold))
+
+den = density(background, from=-8, to=8)
+plot(den$x, den$y, type="l", xlab = "PARS Score", ylab = "Density",
+		xlim=c(-8,8), col="gray", main = "All Genomic Sites")
+mtext(paste("n = ",length(background)," versus n= ",length(EvoFold),sep=""), side=3, line=0.25)
+den = density(EvoFold, na.rm=T, from=-8, to=8)
+lines(den$x, den$y, type = "l", col="blue")
+legend("topleft",legend=c("All","EvoFold"),col=c("gray","blue"),
+		lwd=2, ncol=1, cex=0.8)
+
+##################
+### Exon sites ###
+##################
+
+background = PARS.table$V5[match(PARS.exon.ID,PARS.ID,nomatch=0)]
+print(length(background))
+print(quantile(background))
+
+EvoFold = PARS.table$V5[match(EvoFold_PARS.exon.ID,PARS.ID,nomatch=0)]
+print(length(EvoFold))
+print(quantile(EvoFold))
+
+den = density(background, from=-8, to=8)
+plot(den$x, den$y, type="l", xlab = "PARS Score", ylab = "Density",
+		xlim=c(-8,8), col="gray", main = "Exonic Sites")
+mtext(paste("n = ",length(background)," versus n= ",length(EvoFold),sep=""), side=3, line=0.25)
+den = density(EvoFold, na.rm=T, from=-8, to=8)
+lines(den$x, den$y, type = "l", col="blue")
+legend("topleft",legend=c("All","EvoFold"),col=c("gray","blue"),
+		lwd=2, ncol=1, cex=0.8)
+dev.off()
